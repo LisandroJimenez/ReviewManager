2a0e08c855e3b98891ab2a1fca2dd816cd9950b4
LisandroJimenez <ljimenezv2006@gmail.com>
2025-05-18 23:21:39 -0600

    details

--- src/middlewares/validate-cant-request.js
+++ src/middlewares/validate-cant-request.js
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
-    windowMs: 30 * 60 * 1000,
-    max: 100,
+    windowMs: 50 * 60 * 1000,
+    max: 200,
     message: {
         success: false,
         msg: "Too many requests from this IP, please try again after 15 minutes"
